aae3bc4feac3fbd253617268129d15a1b70c44db
Arthur <arthur.stefe@gmail.com>
2016-10-06 17:53:41 -0300

    Profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006191304) do
+ActiveRecord::Schema.define(version: 20161006193834) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
