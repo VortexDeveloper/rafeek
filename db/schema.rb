9594d7602ff0ccb3af4d83f2f7d539f7275fbd79
= <=>
2016-10-20 14:59:15 -0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018122458) do
+ActiveRecord::Schema.define(version: 20161018201744) do
 
   create_table "accounts", force: :cascade do |t|
-    t.integer  "user_id"
     t.integer  "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user"
+    t.integer  "user_id"
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
@@ -126,11 +127,12 @@ ActiveRecord::Schema.define(version: 20161018122458) do
   end
 
   create_table "ticket_transactions", force: :cascade do |t|
-    t.integer  "amount"
-    t.integer  "status"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "amount"
+    t.integer  "user_id"
+    t.integer  "status"
+    t.integer  "value"
     t.index ["user_id"], name: "index_ticket_transactions_on_user_id"
   end
 
@@ -140,6 +142,7 @@ ActiveRecord::Schema.define(version: 20161018122458) do
     t.datetime "updated_at",            null: false
     t.integer  "owner_id"
     t.integer  "ticket_transaction_id"
+    t.integer  "ticket_transaction"
     t.index ["owner_id"], name: "index_tickets_on_owner_id"
     t.index ["raffle_id"], name: "index_tickets_on_raffle_id"
     t.index ["ticket_transaction_id"], name: "index_tickets_on_ticket_transaction_id"
