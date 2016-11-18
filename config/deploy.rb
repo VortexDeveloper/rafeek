2ad126c70881d7cf7f4d421de836af1570c70695
Thiago Ururay <taraujo@id.uff.br>
2016-11-18 19:37:28 -0200

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ task :setup do
 end
 
 desc "Deploys the current version to the server."
-task :deploy => :production do
+task deploy: :environment do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
   deploy do
