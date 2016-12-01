8f47927be9eabbb0b27cc9b5ccd992a53f1d8c46
= <=>
2016-12-01 17:14:00 -0200

    ajuste nas rifas e na mensagem de compra de pacote

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -1,2 +1,18 @@
 module PackagesHelper
+  def bandeira_for_select
+    options_for_select(bandeiras)
+  end
+
+  def bandeiras
+    {
+      'VISA' => 'visa',
+      "MASTERCARD" => 'mastercard',
+      'DINERS' => 'diners',
+      'DISCOVER' => 'discover',
+      'ELO' => 'elo',
+      'AMEX' => 'amex',
+      'JCB' => 'jcb',
+      'AURA' => 'aura'
+    }
+  end
 end
